5af4ed9a3e7e167055de0b9aaeb7e6b7f5f6ed84
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-08-26 03:06:00 -0500

    chore: update .gitignore to include coverage directory and adjust reportsDirectory in vite.config.ts

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
-      reportsDirectory: './src/coverage'
+      reportsDirectory: './coverage'
     }
   }
 })
