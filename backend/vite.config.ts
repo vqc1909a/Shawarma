7f437fc1c390715a9d27304bc606d97b051fe114
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-08-26 03:03:46 -0500

    Refactor code structure for improved readability and maintainability

---
+++ backend/vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  test: {
+    // Use jsdom for maximum compatibility and realism; use happy-dom for faster, simpler tests where full browser accuracy is not required.
+    environment: "node",
+    setupFiles: "./vitest-setup.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      reportsDirectory: './src/coverage'
+    }
+  }
+})
