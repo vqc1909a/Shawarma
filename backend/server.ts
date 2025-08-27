1d4db43cdbe763dd72d0de0ce4291db5b9a641cd
Victor Cesar Quispe Atencio <vqc1909a@gmail.com>
2025-08-27 18:05:30 -0500

    feat: add production environment configuration and update frontend API URL

--- backend/server.ts
+++ backend/server.ts
@@ -1,19 +1,31 @@
 import express from "express";
 import cors from "cors";
-import { filesRouter } from "./src/routes/filesRoute.ts";
+import { fileURLToPath } from "url";
+import path from "path";
+import {filesRouter} from "./src/routes/filesRoute.ts";
 
 export const app = express();
 const PORT = process.env.PORT || 4000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
-	return res.send("Hello, World!");
-});
-
 app.use("/api/files", filesRouter);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.get("/", function (req, res) {
+		return res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+	});
+} else {
+	app.get("/", (req, res) => {
+		return res.send("Hello, World!");
+	});
+}
 
 app.listen(PORT, () => {
 	console.log(`API is running on http://localhost:${PORT}`);
